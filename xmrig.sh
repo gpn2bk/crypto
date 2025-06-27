24f0bb0650d1f092bd1eec6dbb702efb3de04e25
gpn2bk <gpn2bjks@gmail.com>
2025-06-27 11:17:44 +0800

    Create xmrig.sh

---
+++ xmrig.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+git clone https://github.com/xmrig/xmrig.git
+mkdir xmrig/build && cd xmrig/build
+cmake ..
+make -j$(nproc)
